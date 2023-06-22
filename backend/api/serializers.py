591dbdf02b8c5924c71435fc187bf4c77ae519dc
NITRO <edmondkoko777@gmail.com>
2023-06-22 19:01:17 +0300

    Диплом

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -38,28 +38,20 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context.get('request').user
-        return not user.is_anonymous and Subscription.objects.filter(
-            user=user, author=obj).exists()
+        if user.is_anonymous:
+            return False
+        return Subscription.objects.filter(user=user, author=obj.id).exists()
 
 
 class SubscriptionSerializer(UserSerializer):
-    is_subscribed = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
 
-    class Meta:
-        model = User
-        fields = ('email', 'id',
-                  'username', 'first_name',
-                  'last_name', 'is_subscribed',
-                  'recipes', 'recipes_count')
-
-    def get_is_subscribed(self, value):
-        return (
-                self.context.get('request').user.is_authenticated
-                and Subscription.objects.filter(user=self.context['request'].user,
-                                                author=value).exists()
+    class Meta(UserSerializer.Meta):
+        fields = UserSerializer.Meta.fields + (
+            'recipes_count', 'recipes'
         )
+        read_only_fields = ('email', 'username')
 
     def validate_subscribe(self, value):
         author = self.instance
