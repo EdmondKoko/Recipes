1c594417b4e9a29b3552b275457b02f3ff9c50a4
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-18 20:29:54 +0300

    Update admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -7,6 +7,7 @@ from .models import (Favorite, Ingredient, RecipeIngredient, Recipe,
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+  """Для модели Recipe создана кастомная админка."""
     list_display = ('name', 'id', 'author', 'add_in_favorite')
     readonly_fields = ('add_in_favorite',)
     list_filter = ('name', 'author', 'tags',)
@@ -18,6 +19,7 @@ class RecipeAdmin(admin.ModelAdmin):
 
 @admin.register(Ingredient)
 class IngredientAdmin(admin.ModelAdmin):
+  """Для модели Ingredient создана кастомная админка."""
     list_display = ('name', 'measurement_unit')
     list_filter = ('name',)
     search_fields = ('name',)
@@ -25,19 +27,23 @@ class IngredientAdmin(admin.ModelAdmin):
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
+  """Для модели Tag создана кастомная админка."""
     list_display = ('name', 'color', 'slug',)
 
 
 @admin.register(ShoppingCart)
 class ShoppingCartAdmin(admin.ModelAdmin):
+  """Для модели ShopingCart создана кастомная админка."""
     list_display = ('user', 'recipe',)
 
 
 @admin.register(Favorite)
 class FavoriteAdmin(admin.ModelAdmin):
+  """Для модели Favorite создана кастомная админка."""
     list_display = ('user', 'recipe',)
 
 
 @admin.register(RecipeIngredient)
 class RecipeIngredientAdmin(admin.ModelAdmin):
+  """Для модели RecipeIngredient создана кастомная админка."""
     list_display = ('recipe', 'ingredient', 'amount',)
