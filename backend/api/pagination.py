8c9d7982a62f60c395806533044fe8d9284bd8eb
EdmondKoko <107623627+EdmondKoko@users.noreply.github.com>
2023-10-18 18:57:02 +0300

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -2,4 +2,5 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    """Пагинация с номерами страниц."""
     page_size_query_param = 'limit'
